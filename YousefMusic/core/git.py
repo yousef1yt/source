a489d53f938329f01329100a27fbce277b1c627b
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-03 02:28:13 +0300

    Add files via upload

---
+++ YousefMusic/core/git.py
@@ -0,0 +1,30 @@
+
+import asyncio
+import shlex
+from typing import Tuple
+
+
+import config
+
+from ..logging import LOGGER
+
+
+def install_req(cmd: str) -> Tuple[str, str, int, int]:
+    async def install_requirements():
+        args = shlex.split(cmd)
+        process = await asyncio.create_subprocess_exec(
+            *args,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        return (
+            stdout.decode("utf-8", "replace").strip(),
+            stderr.decode("utf-8", "replace").strip(),
+            process.returncode,
+            process.pid,
+        )
+
+    return asyncio.get_event_loop().run_until_complete(
+        install_requirements()
+    )
---
+++ YousefMusic/core/mongo.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import MONGO_DB_URI
+
+from ..logging import LOGGER
+
+LOGGER("سورس اكس").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER("سورس اكس").info("تم الاتصـال بقاعـدة البيانـات ...✓")
+except:
+    LOGGER(__name__).error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
+    exit()
