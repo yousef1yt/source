a765dd7023acdd87d83688ecfdb27ad8fc82233d
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-03 02:23:05 +0300

    Add files via upload

---
+++ YousefMusic/plugins/misc/watcher.py
@@ -0,0 +1,14 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from YousefMusic import app
+from YousefMusic.core.call import Zelzaly
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await Zelzaly.stop_stream_force(message.chat.id)
