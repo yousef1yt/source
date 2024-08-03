1aaf9a56cd3e211d0c565c34d388797d1004e864
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-03 04:23:34 +0300

    Update Speakers.py

--- YousefMusic/plugins/play/Speakers.py
+++ YousefMusic/plugins/play/Speakers.py
@@ -1,34 +1,64 @@
 from pyrogram import filters, Client
-from MatrixMusic import app
+from YousefMusic import app
 import asyncio
+import config
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import VideoChatEnded, Message
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from MatrixMusic.core.call import Zelzaly 
-from MatrixMusic.utils.database import *
-from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError)
+from YousefMusic.core.call import Dil
+from YousefMusic.utils.database import *
+from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
-@app.on_message(filters.regex("المتكلمين"))
+
+@app.on_message(filters.regex("مين في الكول"))
 async def strcall(client, message):
-    assistant = await group_assistant(Zelzaly,message.chat.id)
+    assistant = await group_assistant(Dil, message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./strings/hossam.mp3"), stream_type=StreamType().pulse_stream)
-        text="↯︙الاعضاء الموجودين في المكالمة المرئية :\n\n"
+        await assistant.join_group_call(message.chat.id, AudioPiped("https://graph.org/file/217aac5f9cd2b05f7ba5a.mp4"), stream_type=StreamType().pulse_stream)
+        text = "- الموجودين في الكول  . :\n\n"
         participants = await assistant.get_participants(message.chat.id)
-        k =0
+        k = 0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتكلم"
+                mut = "- عم يلعي  . "
             else:
-                mut="لا يتكلم"
+                mut = "- ساكت  . "
             user = await client.get_users(participant.user_id)
-            k +=1
-            text +=f"{k}>{user.mention}>{mut}\n"
-        text += f"\nعددهم : {len(participants)}"    
-        await message.reply(f"{text}")
-        await asyncio.sleep(5)
+            k += 1
+            text += f"{k} ~ {user.mention} {mut}\n"
+        text += f"\n~ عددهم : {len(participants)}\n️"  
+
+        # إضافة زر شفاف في الأسفل
+        inline_keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("- قناة البوت . ", url=config.SUPPORT_CHAT)],
+        ])      
+
+        await message.reply(f"{text}", reply_markup=inline_keyboard)
+        await asyncio.sleep(7)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"لم يتم العثور على دردشة فيديو نشطة.\nيرجى بدء دردشة فيديو في مجموعتك/قناتك والمحاولة مرة أخرى.")
+        await message.reply(f"- مفيش حاجة شغاله  .")
     except TelegramServerError:
-        await message.reply(f"ارسل مرة اخرى يوجد خطأ في احد سيرفرات التليكرام")
+        await message.reply(f"- حدث خطأ.")
+    except AlreadyJoinedError:
+        text = "- الموجودين في الكول  . :\n\n"
+        participants = await assistant.get_participants(message.chat.id)
+        k = 0
+        for participant in participants:
+            info = participant
+            if info.muted == False:
+                mut = "- عم يلعي  . "
+            else:
+                mut = "- ساكت  . "
+            user = await client.get_users(participant.user_id)
+            k += 1
+            text += f"{k} ~ {user.mention} {mut}\n"
+        text += f"\n~ عددهم : {len(participants)}\n️"
+
+        # إضافة زر شفاف في الأسفل
+        inline_keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("- قناة البوت . ", url=config.SUPPORT_CHAT)],
+        ])
+        await message.reply(f"{text}", reply_markup=inline_keyboard)
