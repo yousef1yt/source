d9522367498d629cab1524264747f38b930f2ed3
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-03 03:40:31 +0300

    Update download.py

--- YousefMusic/plugins/play/download.py
+++ YousefMusic/plugins/play/download.py
@@ -19,7 +19,7 @@ def remove_if_exists(path):
         os.remove(path)
 
 
-@app.on_message(command(["/song", "تنزيل", "/music"]))
+@app.on_message(command(["نزل", "تنزيل", "يوت"]))
 async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث عـن المقطـع الصـوتـي . . .</b>")
